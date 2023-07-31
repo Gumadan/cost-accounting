5415f85fb511120c8fd7848c97bd13608616024c
Gumadan <fral1488@gmail.com>
2023-07-31 22:47:09 +0400

    changed  file structure, corrected the syntax

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,8 +1,8 @@
-import CostItem from "./CostItem";
+import CostItem from "./Costitem";
 import "./Costs.css";
-import Card from "./Card";
+import Card from "../UI/Card";
 
-function Costs(props) {
+const Costs = (props) => {
   return (
     <Card className="costs">
       <CostItem
