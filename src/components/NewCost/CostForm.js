3c7a6a95e7d9192f141cacb4c84ecb64fecc457c
Gumadan <fral1488@gmail.com>
2023-08-02 23:30:46 +0400

    123

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -1,21 +1,46 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./CostForm.css";
 
 const CostForm = () => {
-  const [name, setName] = useState("");
-  const [amount, setAmount] = useState("");
-  const [date, setDate] = useState("");
+    const [name, setName] = useState("");
+    const [amount, setAmount] = useState("");
+    const [date, setDate] = useState("");
+
+//   const [userInput, setUserInput] = useState({
+//     name: "",
+//     amount: "",
+//     date: "",
+//   });
 
   const nameChangeHandler = (event) => {
     setName(event.target.value);
+    //     setUserInput({
+    //       ...userInput,
+    //       name: event.target.value,
+    //     });
+    //   };
+    // setUserInput((previousState) => {
+    //   return {
+    //     ...previousState,
+    //     name: event.terget.value,
+    //   };
+    // });
   };
 
   const amountChangeHandler = (event) => {
     setAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   amount: event.target.value,
+    // });
   };
 
   const dateChangeHandler = (event) => {
     setDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   date: event.target.value,
+    // });
   };
 
   return (
@@ -27,11 +52,21 @@ const CostForm = () => {
         </div>
         <div className="new-cost__control">
           <label>Сумма</label>
-          <input onChange={amountChangeHandler} type="number" min="0.01" step="0.01" />
+          <input
+            onChange={amountChangeHandler}
+            type="number"
+            min="0.01"
+            step="0.01"
+          />
         </div>
         <div className="new-cost__control">
           <label>Дата</label>
-          <input onChange={dateChangeHandler} type="date" min="2019-01-01" step="2022-12-31" />
+          <input
+            onChange={dateChangeHandler}
+            type="date"
+            min="2019-01-01"
+            step="2022-12-31"
+          />
         </div>
         <div className="new-cost__actions">
           <button type="submit">Добавить расход</button>
